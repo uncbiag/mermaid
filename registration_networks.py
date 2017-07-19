1eb5b2f97de3fb5323ccc2874c3250ff522a74e6
Marc Niethammer <mn@cs.unc.edu>
2017-07-19 13:50:22 -0400

    SVF map version now supports 1D, 2D, and 3D. Still requires testing, cleanup, and CUDA implementation.

--- registration_networks.py
+++ registration_networks.py
@@ -19,6 +19,8 @@ from modules.stn_nd import STN_ND
 
 import utils
 
+# TODO: create separate classes for regularizers and similiarty measures to avoid code duplication
+
 class SVFNet(nn.Module):
 # TODO: do proper spacing
     def __init__(self,sz,spacing,params):
@@ -147,7 +149,7 @@ class SVFNetMap(nn.Module):
 
 
 class SVFLossMap(nn.Module):
-# make velocity field an input here
+    # make velocity field an input here
     def __init__(self,v,sz,spacing,params):
         super(SVFLossMap, self).__init__()
         self.sigma = params['sigma']
@@ -155,6 +157,7 @@ class SVFLossMap(nn.Module):
         self.gamma = params['gamma']
         self.v = v
         self.fdt = fd.FD_torch( spacing )
+        self.spacing = spacing
         self.dim = spacing.size
         self.volumeElement = self.spacing.prod()
         self.stn = STN_ND( self.dim )
@@ -162,10 +165,24 @@ class SVFLossMap(nn.Module):
 
     def computeRegularizer(self, v, alpha, gamma):
         # just do the standard component-wise gamma id -\alpha \Delta
-        ndim = v.ndimension()
-        if ndim != 3:
-            raise ValueError('Regularizer only supported in 2D at the moment')
 
+        if self.dim==1:
+            return self._computeRegularizer_1d(v, alpha, gamma)
+        elif self.dim==2:
+            return self._computeRegularizer_2d(v, alpha, gamma)
+        elif self.dim==3:
+            return self._computeRegularizer_3d(v, alpha, gamma)
+        else:
+            raise ValueError('Regularizer is currently only supported in dimensions 1 to 3')
+
+
+    def _computeRegularizer_1d(self, v, alpha, gamma):
+        Lv = Variable(torch.zeros(v.size()), requires_grad=False)
+        Lv = v* gamma - self.fdt.lap(v) * alpha
+        # now compute the norm
+        return (Lv**2).sum()
+
+    def _computeRegularizer_2d(self, v, alpha, gamma):
         Lv = Variable(torch.zeros(v.size()), requires_grad=False)
         for i in [0, 1]:
             Lv[:, :, i] = v[:, :, i] * gamma - self.fdt.lap(v[:, :, i]) * alpha
@@ -173,24 +190,55 @@ class SVFLossMap(nn.Module):
         # now compute the norm
         return (Lv[:, :, 0] ** 2 + Lv[:, :, 1] ** 2).sum()
 
+    def _computeRegularizer_3d(self, v, alpha, gamma):
+        Lv = Variable(torch.zeros(v.size()), requires_grad=False)
+        for i in [0, 1, 2]:
+            Lv[:, :, :, i] = v[:, :, :, i] * gamma - self.fdt.lap(v[:, :, :, i]) * alpha
+
+        # now compute the norm
+        return (Lv[:, :, :, 0] ** 2 + Lv[:, :, :, 1] ** 2 + Lv[:, :, :, 2] ** 2).sum()
+
+    def computeWarpedImage(self,I0,phi):
+
+        if self.dim == 1:
+            return self._computeWarpedImage_1d(I0,phi)
+        elif self.dim == 2:
+            return self._computeWarpedImage_2d(I0,phi)
+        elif self.dim == 3:
+            return self._computeWarpedImage_3d(I0,phi)
+        else:
+            raise ValueError('Images can only be warped in dimensions 1 to 3')
+
+    def _computeWarpedImage_1d(self,I0,phi):
+        I0_stn = I0.view(torch.Size([1, self.sz, 1]))
+        phi_stn = phi.view(torch.Size([1, self.sz, 1]))
+        I1_warped = self.stn(I0_stn, phi_stn)
+        return I1_warped[0, :]
+
+    def _computeWarpedImage_2d(self,I0,phi):
+        I0_stn = I0.view(torch.Size([1, self.sz, self.sz, 1]))
+        phi_stn = phi.view(torch.Size([1, self.sz, self.sz, 2]))
+        I1_warped = self.stn(I0_stn, phi_stn)
+        return I1_warped[0,:,:,0]
+
+    def _computeWarpedImage_3d(self,I0,phi):
+        I0_stn = I0.view(torch.Size([1, self.sz, self.sz, self.sz, 1]))
+        phi_stn = phi.view(torch.Size([1, self.sz, self.sz, self.sz, 3]))
+        I1_warped = self.stn(I0_stn, phi_stn)
+        return I1_warped[0, :, :, :, 0]
 
     def getEnergy(self, phi1, I0_source, I1_target):
-        I0_source_stn = I0_source.view(torch.Size([1, self.sz, self.sz, 1]))
-        phi1_stn = phi1.view(torch.Size([1, self.sz, self.sz, 2]))
-        I1_warped = self.stn(I0_source_stn, phi1_stn)
+        I1_warped = self.computeWarpedImage(I0_source, phi1)
 
-        ssd = utils.t2np( (((I1_target - I1_warped[0, :, :, 0]) ** 2).sum() / (self.sigma ** 2))[0] )*self.volumeElement
+        ssd = utils.t2np( (((I1_target - I1_warped) ** 2).sum() / (self.sigma ** 2))[0] )*self.volumeElement
         reg = utils.t2np( (self.computeRegularizer(self.v, self.alpha, self.gamma))[0] )*self.volumeElement
         energy = ssd + reg
         return energy, ssd, reg
 
     def forward(self, phi1, I0_source, I1_target):
+        I1_warped = self.computeWarpedImage(I0_source,phi1)
 
-        I0_source_stn = I0_source.view(torch.Size([1,self.sz,self.sz,1]))
-        phi1_stn = phi1.view(torch.Size([1,self.sz,self.sz,2]))
-        I1_warped = self.stn(I0_source_stn, phi1_stn)
-
-        ssd = ((I1_target - I1_warped[0,:,:,0]) ** 2).sum() / (self.sigma ** 2)*self.volumeElement
+        ssd = ((I1_target - I1_warped) ** 2).sum() / (self.sigma ** 2)*self.volumeElement
         reg = self.computeRegularizer( self.v, self.alpha, self.gamma)*self.volumeElement
         energy = ssd + reg
         #print('Energy = ' + str(utils.t2np(energy)[0]) + '; ssd = ' + str(utils.t2np(ssd)[0]) + '; reg = ' + str(utils.t2np(reg)[0]))
--- testSVFMap.py
+++ testSVFMap.py
@@ -10,111 +10,34 @@ Contributors:
 from __future__ import print_function
 import torch
 from torch.autograd import Variable
-from torch.nn.parameter import Parameter
-import torch.nn as nn
-import torch.nn.functional as F
 
-import finite_differences as fd
-import rungekutta_integrators as RK
-import forward_models as FM
 import utils
 import registration_networks as RN
 
 from modules.stn_nd import STN_ND
 
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as img
 
-dim = 2 # dimension
+import visualize_registration_results as vizReg
+import example_generation as eg
+
+# select the desired dimension of the registration
+dim = 2
+sz = 30         # size of the desired images: (sz)^dim
 
 # create a default image size with two sample squares
-sz = 30         # size of the 2D image: sz x sz
-c = sz/2        # center coordinates
-len_s = sz/6    # half of side-length for small square
-len_l = sz/3    # half of side-length for large square
-
-# create two example square images
-I0 = np.zeros([sz,sz], dtype='float32' )
-I1 = np.zeros([sz,sz], dtype='float32' )
-
-# create small and large squares
-I0[c-len_s:c+len_s,c-len_s:c+len_s] = 1
-I1[c-len_l:c+len_l,c-len_l:c+len_l] = 1
+cs = eg.CreateSquares(dim,sz)
+I0,I1 = cs.create_image_pair()
 
 # spacing so that everything is in [0,1]^2 for now
 # TODO: change to support arbitrary spacing
 hx = 1./(sz-1)
-print ('Spacing = ' + str( hx ) )
-
-fdnp = fd.FD_np()       # numpy finite differencing
-fdnp.setspacing(hx,hx)  # set spacing
+spacing = np.tile( hx, dim )
+print ('Spacing = ' + str( spacing ) )
 
 # some debugging output to show image gradients
 # compute gradients
-dx0 = fdnp.dXc( I0 )
-dy0 = fdnp.dYc( I0 )
-
-dx1 = fdnp.dXc( I1 )
-dy1 = fdnp.dYc( I1 )
-
-plt.figure(1)
-plt.setp( plt.gcf(),'facecolor','white')
-plt.style.use('bmh')
-
-plt.subplot(321)
-plt.imshow( I0 )
-plt.title( 'I0' )
-plt.subplot(323)
-plt.imshow( dx0 )
-plt.subplot(325)
-plt.imshow( dy0 )
-
-plt.subplot(322)
-plt.imshow( I1 )
-plt.title( 'I1' )
-plt.subplot(324)
-plt.imshow( dx1 )
-plt.subplot(326)
-plt.imshow( dy1 )
-#plt.axis('tight')
-plt.show( block=False )
-
-
-def showCurrentImages(iter,iS,iT,iW):
-    """
-    Show current 2D registration results in relation to the source and target images
-    :param iter: iteration number
-    :param iS: source image
-    :param iT: target image
-    :param iW: current warped image
-    :return: no return arguments
-    """
-    plt.figure(1)
-    plt.clf()
-
-    plt.suptitle( 'Iteration = ' + str(iter))
-    plt.setp(plt.gcf(), 'facecolor', 'white')
-    plt.style.use('bmh')
-
-    plt.subplot(131)
-    plt.imshow(utils.t2np(iS))
-    plt.colorbar()
-    plt.title('source image')
-
-    plt.subplot(132)
-    plt.imshow(utils.t2np(iT))
-    plt.colorbar()
-    plt.title('target image')
-
-    plt.subplot(133)
-    plt.imshow(utils.t2np(iW))
-    plt.colorbar()
-    plt.title('warped image')
-
-    plt.show(block=False)
-    plt.draw_all(force=True)
-    plt.waitforbuttonpress()
+vizReg.debugOutput( I0, I1, spacing )
 
 # some settings for the registration energy
 # Reg[\Phi,\alpha,\gamma] + 1/\sigma^2 Sim[I(1),I_1]
@@ -123,7 +46,7 @@ params['sigma']=0.1
 params['gamma']=1.
 params['alpha']=0.2
 
-model = RN.SVFNetMap(sz,params)    # instantiate a stationary velocity field model
+model = RN.SVFNetMap(sz,spacing,params)    # instantiate a stationary velocity field model
 print(model)
 
 # create the source and target image as pyTorch variables
@@ -131,23 +54,34 @@ ISource = Variable( torch.from_numpy( I0.copy() ), requires_grad=False )
 ITarget = Variable( torch.from_numpy( I1 ), requires_grad=False )
 
 # create the identity map
-id2 = utils.identityMap([sz,sz])
-
-id = np.zeros([sz,sz,2], dtype='float32')
-id[:,:,0] = id2[0]
-id[:,:,1] = id2[1]
+if dim==1:
+    id1 = utils.identityMap([sz])
+    id = np.zeros([sz], dtype='float32')
+    id = id1
+elif dim==2:
+    id2 = utils.identityMap([sz, sz])
+    id = np.zeros([sz, sz, 2], dtype='float32')
+    id[:, :, 0] = id2[0]
+    id[:, :, 1] = id2[1]
+elif dim==3:
+    id3 = utils.identityMap([sz, sz, sz])
+    id = np.zeros([sz, sz, sz, 3], dtype='float32')
+    id[:, :, :, 0] = id3[0]
+    id[:, :, :, 1] = id3[1]
+    id[:, :, :, 2] = id3[2]
+else:
+    raise ValueError('Can only deal with images of dimension 1-3 so far')
 
 identityMap = Variable( torch.from_numpy( id ), requires_grad=False )
 
-# use the standard SVFLoss
-criterion = RN.SVFLossMap(list(model.parameters())[0],sz,params)
+criterion = RN.SVFLossMap(list(model.parameters())[0],sz,spacing,params) # stationaty velocity field with maps
 # use LBFGS as optimizer; this is essential for convergence when not using the Hilbert gradient
 optimizer = torch.optim.LBFGS(model.parameters(),lr=1)
 
-stn = STN_ND( dim )
+stn = STN_ND( dim ) # spatial transformer so we can apply the map
+
 # optimize for a few steps
 for iter in range(100):
-    print( 'Iteration = ' + str( iter ) )
 
     def closure():
         optimizer.zero_grad()
@@ -155,16 +89,33 @@ for iter in range(100):
         phiWarped = model(identityMap)
         # Compute and print loss
         loss = criterion(phiWarped, ISource, ITarget)
-        print(iter, loss.data[0])
+        #print(iter, loss.data[0])
         loss.backward()
         return loss
 
     optimizer.step(closure)
-    p = list(model.parameters())
-    print( 'v norm = ' + str( (p[0]**2).sum()[0] ) )
+    #p = list(model.parameters())
+    #print( 'v norm = ' + str( (p[0]**2).sum()[0] ) )
     phiWarped = model(identityMap)
+
+    if iter%1==0:
+        energy, ssdEnergy, regEnergy = criterion.getEnergy(phiWarped, ISource, ITarget)
+        print('Iter {iter}: E={energy}, ssdE={ssdE}, regE={regE}'
+              .format(iter=iter, energy=energy, ssdE=ssdEnergy, regE=regEnergy))
+
     if iter%10==0:
-        phiWarped_stn = phiWarped.view(torch.Size([1, sz, sz, 2]))
-        ISource_stn = ISource.view(torch.Size([1,sz,sz,1]))
-        I1Warped = stn(ISource_stn, phiWarped_stn)
-        showCurrentImages(iter,ISource,ITarget,I1Warped[0,:,:,0])
+        if dim==1:
+            phiWarped_stn = phiWarped.view(torch.Size([1, sz]))
+            ISource_stn = ISource.view(torch.Size([1, sz, 1]))
+            I1Warped = stn(ISource_stn, phiWarped_stn)
+            vizReg.showCurrentImages(iter, ISource, ITarget, I1Warped[0, :, 0])
+        elif dim==2:
+            phiWarped_stn = phiWarped.view(torch.Size([1, sz, sz, 2]))
+            ISource_stn = ISource.view(torch.Size([1,sz,sz,1]))
+            I1Warped = stn(ISource_stn, phiWarped_stn)
+            vizReg.showCurrentImages(iter,ISource,ITarget,I1Warped[0,:,:,0])
+        elif dim==3:
+            phiWarped_stn = phiWarped.view(torch.Size([1, sz, sz, sz, 3]))
+            ISource_stn = ISource.view(torch.Size([1, sz, sz, sz, 1]))
+            I1Warped = stn(ISource_stn, phiWarped_stn)
+            vizReg.showCurrentImages(iter, ISource, ITarget, I1Warped[0, :, :, :, 0])
