8a640c3f5935da776217f2a4b470ce3137ae3ed9
zyshen <zyshen021@gmail.com>
2017-12-12 17:54:51 -0500

    'update'

---
+++ pyreg/data_pool.py
@@ -0,0 +1,365 @@
+from __future__ import print_function
+
+from torch.utils.data import Dataset
+
+from pyreg.data_utils import *
+
+
+class BaseDataSet(object):
+
+    def __init__(self, name, dataset_type, file_type_list, sched=None):
+        """
+
+        :param name: name of data set
+        :param dataset_type: ''mixed' like oasis including inter and  intra person  or 'custom' like LPBA40, only includes inter person
+        :param file_type_list: the file type to be includes like [*1_a.bmp, *2_a.bmp]
+        :param data_path: path of the dataset
+        """
+        self.name = name
+        self.data_path = None
+        self.output_path = None
+        self.pair_name_list = []
+        self.pair_path_list = []
+        self.file_type_list = file_type_list
+        self.save_format = 'h5py'
+        self.sched = sched
+        self.dataset_type = dataset_type
+        self.normalize_sched = 'ntp'
+        self.divided_ratio = (0.7, 0.1, 0.2)
+
+    def generate_pair_list(self):
+        pass
+
+
+
+    def set_data_path(self, path):
+        self.data_path = path
+
+    def set_output_path(self, path):
+        self.output_path = path
+        make_dir(path)
+
+    def set_normalize_sched(self,sched):
+        self.normalize_sched = sched
+
+    def set_divided_ratio(self,ratio):
+        self.divided_ratio = ratio
+
+    def get_file_num(self):
+        return len(self.pair_path_list)
+
+    def get_pair_name_list(self):
+        return self.pair_name_list
+
+    def read_file(self, file_path):
+        """
+        currently, typical to medical image problem
+        :param file_path:
+        :return:
+        """
+        img = read_itk_img(file_path)
+        info = {'img_size':img.shape}
+        return img, info
+    def extract_pair_info(self, info1, info2):
+        return info1
+
+    def save_pair_to_file(self):
+        pass
+
+
+    def prepare_data(self):
+        """
+        preprocessig  data for each dataset
+        :return:
+        """
+        print("starting preapare data..........")
+        print("the output file path is: {}".format(self.output_path))
+        self.pair_path_list = self.generate_pair_list()
+        print("the total num of pair is {}".format(self.get_file_num()))
+        self.save_pair_to_file()
+        print("data preprocessing finished")
+
+
+
+
+
+
+
+class UnlabeledDatSet(BaseDataSet):
+    def __init__(self, name, dataset_type, file_type_list, sched=None):
+        BaseDataSet.__init__(self,name, dataset_type, file_type_list,sched)
+
+    def save_pair_to_file(self):
+        """
+        save the file into h5py
+        :param pair_path_list: N*2  [[full_path_img1, full_path_img2],[full_path_img2, full_path_img3]
+        :param pair_name_list: N*1 for 'mix': [folderName1_sliceName1_folderName2_sliceName2, .....]  for custom: [sliceName1_sliceName2, .....]
+        :param ratio:  divide dataset into training val and test, based on ratio, e.g [0.7, 0.1, 0.2]
+        :param saving_path_list: N*1 list of path for output files e.g [ouput_path/train/sliceName1_sliceName2.h5py,.........]
+        :param info: dic including pair information
+        :param normalized_sched: normalized the image
+        """
+        random.shuffle(self.pair_path_list)
+        self.pair_name_list = generate_pair_name(self.pair_path_list, sched=self.dataset_type)
+        saving_path_list = divide_data_set(self.output_path, self.pair_name_list, self.divided_ratio)
+        img_size = ()
+        for i, pair in enumerate(self.pair_path_list):
+            img1, info1 = self.read_file(pair[0])
+            img2, info2 = self.read_file(pair[1])
+            if i == 0:
+                img_size = img1.shape
+                check_same_size(img2, img_size)
+            else:
+                check_same_size(img1, img_size)
+                check_same_size(img2, img_size)
+                normalize_img(img1, self.normalize_sched)
+                normalize_img(img2, self.normalize_sched)
+            img_pair = np.asarray([(img1, img2)])
+            info = self.extract_pair_info(info1, info2)
+            save_to_h5py(saving_path_list[i], img_pair, info, [self.pair_name_list[i]], verbose=False)
+
+
+
+
+class LabeledDataSet(BaseDataSet):
+    def __init__(self, name, dataset_type, file_type_list, sched=None):
+        BaseDataSet.__init__(self, name, dataset_type, file_type_list,sched)
+        self.label_path = None
+        self.pair_label_path_list=[]
+
+
+    def set_label_path(self, path):
+        self.label_path = path
+
+
+
+    def save_pair_to_file(self):
+        """
+        save the file into h5py
+        :param pair_path_list: N*2  [[full_path_img1, full_path_img2],[full_path_img2, full_path_img3]
+        :param pair_name_list: N*1 for 'mix': [folderName1_sliceName1_folderName2_sliceName2, .....]  for custom: [sliceName1_sliceName2, .....]
+        :param ratio:  divide dataset into training val and test, based on ratio, e.g [0.7, 0.1, 0.2]
+        :param saving_path_list: N*1 list of path for output files e.g [ouput_path/train/sliceName1_sliceName2.h5py,.........]
+        :param info: dic including pair information
+        :param normalized_sched: normalized the image
+        """
+        random.shuffle(self.pair_path_list)
+        self.pair_label_path_list = find_corr_map(self.pair_path_list, self.label_path)
+        self.pair_name_list = generate_pair_name(self.pair_path_list, sched=self.dataset_type)
+        saving_path_list = divide_data_set(self.output_path, self.pair_name_list, self.divided_ratio)
+        img_size = ()
+        for i, pair in enumerate(self.pair_path_list):
+            img1, info1 = self.read_file(pair[0])
+            img2, info2 = self.read_file(pair[1])
+            label1, linfo1 = self.read_file(self.pair_label_path_list[i][0])
+            label2, linfo2 = self.read_file(self.pair_label_path_list[i][1])
+            if i == 0:
+                img_size = img1.shape
+                check_same_size(img2, img_size)
+                check_same_size(label1, img_size)
+                check_same_size(label2, img_size)
+            else:
+                check_same_size(img1, img_size)
+                check_same_size(img2, img_size)
+                check_same_size(label1, img_size)
+                check_same_size(label2, img_size)
+                normalize_img(img1, self.normalize_sched)
+                normalize_img(img2, self.normalize_sched)
+            img_pair = np.asarray([(img1, img2)])
+            label_pair = np.asarray([(label1,label2)])
+            info = self.extract_pair_info(info1, info2)
+            save_to_h5py(saving_path_list[i], img_pair, info, [self.pair_name_list[i]], label_pair, verbose=False)
+
+
+
+
+
+class CustomDataSet(BaseDataSet):
+    def __init__(self,name, dataset_type, file_type_list,full_comb=False):
+        BaseDataSet.__init__(self, name, dataset_type, file_type_list)
+        self.full_comb = full_comb
+
+
+    def generate_pair_list(self, sched=None):
+        """
+        :param sched:
+        :return:
+        """
+        pair_path_list = inter_pair(self.data_path, self.file_type_list, self.full_comb, mirrored=True)
+        return pair_path_list
+
+
+class VolumticDataSet(BaseDataSet):
+    def __init__(self,name, dataset_type, file_type_list):
+        BaseDataSet.__init__(self, name, dataset_type, file_type_list)
+        self.slicing = -1
+
+    def set_slicing(self, slicing):
+        if slicing >0:
+            print("slcing is set on , the slice of {} th dimension would be sliced ".format(slicing))
+        self.slicing = slicing
+
+
+    def read_file(self, file_path, verbose=False):
+        if self.slicing != -1:
+            if verbose:
+                print("slicing file: {}".format(file_path))
+            img = read_itk_img_slice(file_path, self.slicing)
+        else:
+            img= BaseDataSet.read_file(file_path)
+        info = {'img_size': img.shape}
+        return img, info
+
+
+
+
+class MixedDataSet(BaseDataSet):
+    def __init__(self, name, dataset_type, file_type_list, sched, full_comb=False):
+        BaseDataSet.__init__(self, name,dataset_type, file_type_list, sched=sched)
+        self.full_comb = full_comb
+
+
+    def generate_pair_list(self):
+        """
+         return the list of  paths of the paired image  [N,2]
+        :param file_type_list: filter and get the image of certain type
+        :param full_comb: if full_comb, return all possible pairs, if not, return pairs in increasing order
+        :param sched: sched can be inter personal or intra personal
+        :return:
+        """
+        if self.sched == 'intra':
+            dic_list = list_dic(self.data_path)
+            pair_path_list = intra_pair(self.data_path, dic_list, self.file_type_list, self.full_comb, mirrored=True)
+        elif self.sched == 'inter':
+            pair_path_list = inter_pair(self.data_path, self.file_type_list, self.full_comb, mirrored=True)
+        else:
+            raise ValueError("schedule should be 'inter' or 'intra'")
+
+        return pair_path_list
+
+
+
+class OasisDataSet(UnlabeledDatSet, MixedDataSet):
+    """"
+    sched:  'inter': inter_personal,  'intra': intra_personal
+    """
+    def __init__(self, name, sched, full_comb=False):
+        file_type_list = ['*a.mhd'] if sched == 'intra' else ['*_1_a.mhd', '*_2_a.mhd', '*_3_a.mhd']
+        UnlabeledDatSet.__init__(self, name, 'mixed', file_type_list, sched)
+        MixedDataSet.__init__(self, name, 'mixed', file_type_list, sched, full_comb)
+
+
+
+
+class LPBADataSet(VolumticDataSet,LabeledDataSet, CustomDataSet):
+    def __init__(self, name, full_comb=True):
+        VolumticDataSet.__init__(self, name, 'custom', ['*.nii'])
+        LabeledDataSet.__init__(self, name, 'custom', ['*.nii'])
+        CustomDataSet.__init__(self,name, 'custom', ['*.nii'], full_comb)
+
+
+
+
+class IBSRDataSet(VolumticDataSet,LabeledDataSet, CustomDataSet):
+    def __init__(self, name, full_comb=True):
+        VolumticDataSet.__init__(self, name, 'custom', ['*.nii'])
+        LabeledDataSet.__init__(self, name, 'custom', ['*.nii'])
+        CustomDataSet.__init__(self, name, 'custom', ['*.nii'], full_comb)
+
+
+
+class CUMCDataSet(VolumticDataSet,LabeledDataSet, CustomDataSet):
+    def __init__(self, name, full_comb=True):
+        VolumticDataSet.__init__(self, name, 'custom', ['*.nii'])
+        LabeledDataSet.__init__(self, name, 'custom', ['*.nii'])
+        CustomDataSet.__init__(self, name, 'custom', ['*.nii'], full_comb)
+
+
+
+
+if __name__ == "__main__":
+    pass
+
+    # #########################       OASIS TESTING           ###################################3
+    #
+    # path = '/playpen/zyshen/data/oasis'
+    # name = 'oasis'
+    # divided_ratio = (0.6, 0.2, 0.2)
+    #
+    # ###################################################
+    # #oasis  intra testing
+    # file_type_list = ['*a.mhd']
+    # full_comb = True
+    # sched= 'intra'
+    #
+    # output_path = '/playpen/zyshen/data/'+ name+'_pre_'+ sched
+    # oasis = OasisDataSet(name='oasis',sched=sched, full_comb=True)
+    # oasis.set_data_path(path)
+    # oasis.set_output_path(output_path)
+    # oasis.set_divided_ratio(divided_ratio)
+    # oasis.prepare_data()
+
+
+    # ###################################################
+    # # oasis inter testing
+    # file_type_list = ['*_1_a.mhd', '*_2_a.mhd', '*_3_a.mhd']
+    # sched='inter'
+    # full_comb = False
+    # output_path = '/playpen/zyshen/data/' + name + '_pre_' + sched
+    # oasis = OasisDataSet(name='oasis', sched=sched, full_comb=False)
+    # oasis.set_data_path(path)
+    # oasis.set_output_path(output_path)
+    # oasis.set_divided_ratio(divided_ratio)
+    # oasis.prepare_data()
+
+
+
+
+    # ###########################       LPBA TESTING           ###################################
+    # path = '/playpen/data/quicksilver_data/testdata/LPBA40/brain_affine_icbm'
+    # label_path = '/playpen/data/quicksilver_data/testdata/LPBA40/label_affine_icbm'
+    # file_type_list = ['*.nii']
+    # full_comb = False
+    # name = 'lpba'
+    # output_path = '/playpen/zyshen/data/' + name + '_pre'
+    # divided_ratio = (0.6, 0.2, 0.2)
+    #
+    # ###################################################
+    # #lpba testing
+    #
+    # sched= 'intra'
+    #
+    # lpba = LPBADataSet(name=name, full_comb=full_comb)
+    # lpba.set_data_path(path)
+    # lpba.set_output_path(output_path)
+    # lpba.set_divided_ratio(divided_ratio)
+    # lpba.set_label_path(label_path)
+    # lpba.prepare_data()
+
+
+    ###########################       LPBA Slicing TESTING           ###################################
+    path = '/playpen/data/quicksilver_data/testdata/LPBA40/brain_affine_icbm'
+    label_path = '/playpen/data/quicksilver_data/testdata/LPBA40/label_affine_icbm'
+    file_type_list = ['*.nii']
+    full_comb = False
+    name = 'lpba'
+    output_path = '/playpen/zyshen/data/' + name + '_pre_slicing'
+    divided_ratio = (0.6, 0.2, 0.2)
+
+    ###################################################
+    #lpba testing
+
+    sched= 'intra'
+
+    lpba = LPBADataSet(name=name, full_comb=full_comb)
+    lpba.set_slicing(90)
+    lpba.set_data_path(path)
+    lpba.set_output_path(output_path)
+    lpba.set_divided_ratio(divided_ratio)
+    lpba.set_label_path(label_path)
+    lpba.prepare_data()
+
+
+
+
+
--- pyreg/data_utils.py
+++ pyreg/data_utils.py
@@ -9,6 +9,7 @@ import skimage
 from data_utils import *
 import SimpleITK as sitk
 import sys
+import random
 PYTHON_VERSION = 3
 if sys.version_info[0] < 3:
     PYTHON_VERSION = 2
@@ -39,9 +40,14 @@ def list_pairwise(path, img_type, skip, sched):
         raise ValueError("schedule should be 'inter' or 'intra'")
     return pair_list
 
+def check_full_comb_on(full_comb):
+    if full_comb == False:
+        print("only return the pair in order, to get more pairs, set the 'full_comb' True")
+    else:
+        print(" 'full_comb' is on, if you don't need all possible pair, set the 'full_com' False")
 
 
-def inter_pair(path, type, skip=False):
+def inter_pair(path, type, skip=False, mirrored=False):
     """
     get the paired filename list
     :param path: dic path
@@ -49,6 +55,7 @@ def inter_pair(path, type, skip=False):
     :param skip: if skip, return all possible pairs, if not, return pairs in increasing order
     :return: [N,2]
     """
+    check_full_comb_on(skip)
     pair_list=[]
     for sub_type in type:
         f_path = join(path,'**', sub_type)
@@ -65,19 +72,24 @@ def inter_pair(path, type, skip=False):
         if not skip:
             pair = [[f_filter[idx], f_filter[idx + 1]] for idx in range(f_num - 1)]
         else:  # too many pairs , easy to out of memory
-            raise ValueError("Warnning, too many pairs, be sure the memory is big enough. Comment this line if you want to continue ")
+            raise ValueError("Warnning, too many pairs, be sure the disk is big enough. Comment this line if you want to continue ")
             pair = []
             for i in range(f_num - 1):
                 pair_tmp = [[f_filter[i], f_filter[idx + i]] for idx in range(1,f_num - i)]
                 pair += pair_tmp
         pair_list += pair
+    if mirrored:
+        pair_list = mirror_pair(pair_list)
     return pair_list
 
 
 
+def mirror_pair(pair_list):
+    return pair_list + [[pair[1],pair[0]] for pair in pair_list]
 
 
-def intra_pair(path, dic_list, type, skip):
+
+def intra_pair(path, dic_list, type, skip, mirrored=False):
     """
 
     :param path: dic path
@@ -86,6 +98,7 @@ def intra_pair(path, dic_list, type, skip):
     :param skip:  if skip, return all possible pairs, if not, return pairs in increasing order
     :return: [N,2]
     """
+    check_full_comb_on(skip)
     pair_list = []
     for dic in dic_list:
         if PYTHON_VERSION == 3:
@@ -108,6 +121,8 @@ def intra_pair(path, dic_list, type, skip):
                 pair_tmp = [[f_filter[i], f_filter[idx + i]] for idx in range(1,f_num - i)]
                 pair += pair_tmp
         pair_list += pair
+    if mirrored:
+        pair_list = mirror_pair(pair_list)
     return pair_list
 
 
@@ -122,8 +137,8 @@ def load_as_data(pair_list):
     standard=()
 
     for i, pair in enumerate(pair_list):
-        img1 =read_img(pair[0])
-        img2 =read_img(pair[1])
+        img1 =read_itk_img(pair[0])
+        img2 =read_itk_img(pair[1])
 
         # check img size
         if i==0:
@@ -144,6 +159,69 @@ def load_as_data(pair_list):
     return np.asarray(img_pair_list), info, img_pair_path_list
 
 
+def find_corr_map(pair_path_list, label_path):
+    return [[os.path.join(label_path, os.path.split(pth)[1]) for pth in pair_path] for pair_path in pair_path_list]
+
+
+
+def make_dir(path):
+    is_exist = os.path.exists(path)
+    if not is_exist:
+        os.makedirs(path)
+    return is_exist
+
+def divide_data_set(root_path, pair_name_list, ratio):
+    """
+    divide the dataset into root_path/train root_path/val root_path/test
+    :param root_path: the root path for saving the task_dataset
+    :param pair_name_list: list of name of the saved pair  like img1_img2
+    :param ratio: tuple of (train_ratio, val_ratio, test_ratio) from all the pairs
+    :return:  full path of each file
+
+    """
+    train_ratio = ratio[0]
+    val_ratio = ratio[1]
+    pair_num = len(pair_name_list)
+    sub_path = {x:os.path.join(root_path,x) for x in ['train', 'val', 'test']}
+    nt = [make_dir(sub_path[key]) for key in sub_path]
+    if sum(nt):
+        raise ValueError, "the data has already exist, due to randomly assignment schedule, the program block\n" \
+                          "manually delete the folder to reprepare the data"
+    train_num = int(train_ratio * pair_num)
+    val_num = int(val_ratio*pair_num)
+    pair_name_sub_list={}
+    pair_name_sub_list['train'] = pair_name_list[:train_num]
+    pair_name_sub_list['val'] = pair_name_list[train_num: train_num+val_num]
+    pair_name_sub_list['test'] = pair_name_list[train_num+val_num:]
+    saving_path_list = [os.path.join(sub_path[x],pair_name+'.h5py') for x in ['train', 'val', 'test'] for pair_name in pair_name_sub_list[x] ]
+    return saving_path_list
+
+
+
+
+
+
+def generate_pair_name(pair_path_list,sched='mixed'):
+    if sched == 'mixed':
+        return mixed_pair_name(pair_path_list)
+    elif sched == 'custom':
+        return custom_pair_name(pair_path_list)
+
+
+def mixed_pair_name(pair_path_list):
+    f = lambda name: os.path.split(name)
+    get_in = lambda x: os.path.splitext(f(x)[1])[0]
+    get_fn = lambda x: f(f(x)[0])[1]
+    get_img_name = lambda x: get_fn(x)+'_'+get_in(x)
+    img_pair_name_list = [get_img_name(pair_path[0])+'_'+get_img_name(pair_path[1]) for pair_path in pair_path_list]
+    return img_pair_name_list
+
+def custom_pair_name(pair_path_list):
+    f = lambda name: os.path.split(name)
+    get_img_name = lambda x: os.path.splitext(f(x)[1])[0]
+    img_pair_name_list = [get_img_name(pair_path[0])+'_'+get_img_name(pair_path[1]) for pair_path in pair_path_list]
+    return img_pair_name_list
+
 def check_same_size(img, standard):
     """
     make sure all the image are of the same size
@@ -152,24 +230,25 @@ def check_same_size(img, standard):
     """
     assert img.shape == standard, "img size must be the same"
 
-def normalize_img(image, ntp=False):
+def normalize_img(image, sched='ntp'):
     """
     normalize image,
     warning, default [-1,1], which would be tricky when dealing with the bilinear,
-    which default background is 0, corresponding corrections have been done in this application
-    :param image:
-    :param ntp: True: normalized into [-1,1], False: normalized into [0,1]
-    :return: normalized image
+    which default background is 0
+    :param sched: 'ntp': percentile 0.95 then normalized to [-1,1] 'tp': percentile then [0,1], 'p' percentile
+\   :return: normalized image
     """
-    if ntp:
+    if sched == 'ntp':
         image[:] = image / np.percentile(image, 95) * 0.95
         image[:] = 2 * (image - np.min(image)) / (np.max(image) - np.min(image)) - 1
-    else:
+    elif sched == 'tp':
         image[:] = image / np.percentile(image, 95) * 0.95
         image[:] = (image - np.min(image)) / (np.max(image) - np.min(image))
+    elif sched == 'p':
+        image[:] = image / np.percentile(image, 95) * 0.95
 
 
-def read_img(path):
+def read_itk_img(path):
     """
     :param path:
     :return: numpy image
@@ -179,6 +258,16 @@ def read_img(path):
     ct_scan = sitk.GetArrayFromImage(itkimage)
     return np.squeeze(ct_scan)
 
+
+def read_itk_img_slice(path, slicing):
+    """
+    :param path:
+    :return: numpy image
+    """
+    itkimage = sitk.ReadImage(path)
+    # Convert the image to a  numpy array first and then shuffle the dimensions to get axis in the order z,y,x
+    ct_scan = np.squeeze(sitk.GetArrayFromImage(itkimage))
+    return ct_scan[slicing]
 
 
 def read_file(path, type='h5py'):
@@ -202,7 +291,7 @@ def read_file(path, type='h5py'):
 def write_file(path, dic, type='h5py'):
     """
 
-    :param path:
+    :param path: file path
     :param dic:  which has three item : numpy 'data',  dic 'info' , string list 'pair_path'
     :param type:
     :return:
@@ -210,6 +299,8 @@ def write_file(path, dic, type='h5py'):
     if type == 'h5py':
         f = h5py.File(path, 'w')
         f.create_dataset('data',data=dic['data'])
+        if dic['label'] is not None:
+            f.create_dataset('label', data= dic['label'] )
         for key, value in dic['info'].items():
             f.attrs[key] = value
         asciiList = [[path.encode("ascii", "ignore") for path in pair] for pair in dic['pair_path']]
@@ -221,20 +312,22 @@ def write_file(path, dic, type='h5py'):
 
 
 
-def save_to_h5py(path, img_pair_list, info, img_pair_path_list):
+def save_to_h5py(path, img_pair_list, info, img_pair_path_list, label_pair_list=None,verbose=True):
     """
 
-    :param path:
-    :param img_pair_list: numpy data
-    :param info: dictionary for data info
+    :param path:  path for saving file
+    :param img_pair_list: list of image pair
+    :param info: additional info
+    :param img_pair_path_list:  list of path/name of image pair
     :return:
     """
-    dic = {'data': img_pair_list, 'info': info, 'pair_path':img_pair_path_list}
+    dic = {'data': img_pair_list, 'info': info, 'pair_path':img_pair_path_list, 'label': label_pair_list}
     write_file(path, dic, type='h5py')
-    print('data saved: {}'.format(path))
-    print(dic['info'])
-    print("the shape of pair{}".format(dic['data'][:].shape))
-    print('the location of the first file pair\n{}'.format(img_pair_path_list[0]))
+    if verbose:
+        print('data saved: {}'.format(path))
+        print(dic['info'])
+        print("the shape of pair{}".format(dic['data'][:].shape))
+        print('the location of the first file pair\n{}'.format(img_pair_path_list[0]))
 
 
 
