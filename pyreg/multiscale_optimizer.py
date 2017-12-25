0ad753b51f7f5da6cce89c1cffa65a63ae4b8210
zyshen <zyshen021@gmail.com>
2017-12-25 17:03:22 -0500

    'fixed'

--- pyreg/multiscale_optimizer.py
+++ pyreg/multiscale_optimizer.py
@@ -736,7 +736,7 @@ class SingleScaleRegistrationOptimizer(ImageRegistrationOptimizer):
             self.rec_IWarped = self.model(self.ISource)
             loss = self.criterion(self.rec_IWarped, self.ISource, self.ITarget)
         loss.backward()
-        torch.nn.utils.clip_grad_norm(self.model.parameters(), 0.5)
+        #torch.nn.utils.clip_grad_norm(self.model.parameters(), 0.5)
 
 
         if self.useMap:
