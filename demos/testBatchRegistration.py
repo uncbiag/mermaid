Fixed Gaussian computation.

demos/testBatchRegistration.py
@@ -38,15 +38,15 @@ def get_image_range(im_from,im_to):
     return f
 
 # load a bunch of images as source
-I0,hdr,spacing0,_ = im_io.read_batch_to_nc_format(get_image_range(0,10))
+I0,hdr,spacing0,_ = im_io.read_batch_to_nc_format(get_image_range(0,5))
 sz = np.array(I0.shape)
 # and a bunch of images as target images
-I1,hdr,spacing1,_ = im_io.read_batch_to_nc_format(get_image_range(10,20))
+I1,hdr,spacing1,_ = im_io.read_batch_to_nc_format(get_image_range(5,10))
 
 assert( np.all(spacing0==spacing1) )
 
 si.RegisterImagePair().register_images(I0,I1,spacing0,
-                                       model_name='lddmm_shooting_map',
+                                       model_name='lddmm_shooting_scalar_momentum_map',
                                        nr_of_iterations=100,
                                        visualize_step=5,
                                        map_low_res_factor=0.5,

pyreg/custom_pytorch_extensions.py
@@ -457,7 +457,7 @@ class GaussianFourierFilterGenerator(object):
 
         self.mus = np.zeros(self.dim)
         # TODO: storing the identity map may be a little wasteful
-        self.id = utils.identity_map(self.sz,self.spacing)
+        self.centered_id = utils.centered_identity_map(self.sz,self.spacing)
 
         self.complex_gaussian_fourier_filters = [None] * self.nr_of_gaussians
         self.max_indices = [None]*self.nr_of_gaussians
@@ -475,7 +475,7 @@ class GaussianFourierFilterGenerator(object):
     def _compute_complex_gaussian_fourier_filter(self,sigma):
 
         stds = sigma * np.ones(self.dim)
-        gaussian_spatial_filter = utils.compute_normalized_gaussian(self.id, self.mus, stds)
+        gaussian_spatial_filter = utils.compute_normalized_gaussian(self.centered_id, self.mus, stds)
         complex_gaussian_fourier_filter,max_index = create_complex_fourier_filter(gaussian_spatial_filter,self.sz,True)
         return complex_gaussian_fourier_filter,max_index
 
@@ -486,8 +486,8 @@ class GaussianFourierFilterGenerator(object):
 
         # TODO: maybe compute this jointly with the gaussian filter itself to avoid computing the spatial filter twice
         stds = sigma * np.ones(self.dim)
-        gaussian_spatial_filter = utils.compute_normalized_gaussian(self.id, self.mus, stds)
-        gaussian_spatial_xsqr_filter = gaussian_spatial_filter*(self.id**2).sum(axis=0)
+        gaussian_spatial_filter = utils.compute_normalized_gaussian(self.centered_id, self.mus, stds)
+        gaussian_spatial_xsqr_filter = gaussian_spatial_filter*(self.centered_id**2).sum(axis=0)
 
         complex_gaussian_fourier_xsqr_filter,max_index = create_complex_fourier_filter(gaussian_spatial_xsqr_filter,self.sz,True,max_index)
         return complex_gaussian_fourier_xsqr_filter,max_index
@@ -945,8 +945,8 @@ def check_fourier_conv():
     mus = np.zeros(dim)
     stds = np.ones(dim)
     spacing = np.ones(dim)
-    id = utils.identity_map(sz,spacing)
-    g = 100 * utils.compute_normalized_gaussian(id, mus, stds)
+    centered_id = utils.centered_identity_map(sz,spacing)
+    g = 100 * utils.compute_normalized_gaussian(centered_id, mus, stds)
     FFilter,_ = create_complex_fourier_filter(g, sz)
     input = AdaptVal(Variable(torch.randn([1, 1] + list(sz)), requires_grad=True))
     test = gradcheck(FourierConvolution(FFilter), input, eps=1e-6, atol=1e-4)

pyreg/smoother_factory.py
@@ -192,8 +192,8 @@ class GaussianSpatialSmoother(GaussianSmoother):
     def _create_smoothing_kernel(self, k_sz):
         mus = np.zeros(self.dim)
         stds = np.ones(self.dim)
-        id = utils.identity_map(k_sz,self.spacing)
-        g = utils.compute_normalized_gaussian(id, mus, stds)
+        centered_id = utils.centered_identity_map(k_sz,self.spacing)
+        g = utils.compute_normalized_gaussian(centered_id, mus, stds)
 
         return g
 
@@ -388,8 +388,8 @@ class SingleGaussianFourierSmoother(GaussianFourierSmoother):
 
         mus = np.zeros(self.dim)
         stds = self.gaussianStd*np.ones(self.dim)
-        id = utils.identity_map(self.sz,self.spacing)
-        g = utils.compute_normalized_gaussian(id, mus, stds)
+        centered_id = utils.centered_identity_map(self.sz,self.spacing)
+        g = utils.compute_normalized_gaussian(centered_id, mus, stds)
 
         self.FFilter,_ = ce.create_complex_fourier_filter(g, self.sz)
 
@@ -439,7 +439,7 @@ class MultiGaussianFourierSmoother(GaussianFourierSmoother):
     def _create_filter(self):
 
         mus = np.zeros(self.dim)
-        id = utils.identity_map(self.sz,self.spacing)
+        centered_id = utils.centered_identity_map(self.sz,self.spacing)
 
         assert len(self.multi_gaussian_stds)>0
         assert len(self.multi_gaussian_weights)>0
@@ -448,7 +448,7 @@ class MultiGaussianFourierSmoother(GaussianFourierSmoother):
 
         for nr in range(nr_of_gaussians):
             stds = self.multi_gaussian_stds[nr] * np.ones(self.dim)
-            g = self.multi_gaussian_weights[nr] * utils.compute_normalized_gaussian(id, mus, stds)
+            g = self.multi_gaussian_weights[nr] * utils.compute_normalized_gaussian(centered_id, mus, stds)
 
             if nr==0:
                 self.FFilter,_ = ce.create_complex_fourier_filter(g, self.sz)

pyreg/utils.py
@@ -377,6 +377,32 @@ def create_ND_scalar_field_parameter_multiNC(sz, nrOfI=1, nrOfC=1):
     csz = np.array([nrOfI,nrOfC]+list(csz))
     return Parameter(AdaptVal(torch.zeros(csz.tolist())))
 
+
+def centered_identity_map_multiN(sz, spacing):
+    """
+    Create a centered identity map (shifted so it is centered around 0)
+
+    :param sz: size of an image in BxCxXxYxZ format
+    :return: returns the identity map
+    """
+    dim = len(sz) - 2
+    nrOfI = sz[0]
+
+    if dim == 1:
+        id = np.zeros([nrOfI, 1, sz[2]], dtype='float32')
+    elif dim == 2:
+        id = np.zeros([nrOfI, 2, sz[2], sz[3]], dtype='float32')
+    elif dim == 3:
+        id = np.zeros([nrOfI, 3, sz[2], sz[3], sz[4]], dtype='float32')
+    else:
+        raise ValueError('Only dimensions 1-3 are currently supported for the identity map')
+
+    for n in range(nrOfI):
+        id[n, ...] = centered_identity_map(sz[2::], spacing)
+
+    return id
+
+
 def identity_map_multiN(sz,spacing):
     """
     Create an identity map
@@ -400,6 +426,52 @@ def identity_map_multiN(sz,spacing):
         id[n,...] = identity_map(sz[2::],spacing)
 
     return id
+
+
+def centered_identity_map(sz, spacing):
+    """
+    Returns a centered identity map (with 0 in the middle)
+
+    :param sz: just the spatial dimensions, i.e., XxYxZ
+    :return: returns the identity map of dimension dimxXxYxZ
+    """
+    dim = len(sz)
+    if dim == 1:
+        id = np.mgrid[0:sz[0]]
+    elif dim == 2:
+        id = np.mgrid[0:sz[0], 0:sz[1]]
+    elif dim == 3:
+        id = np.mgrid[0:sz[0], 0:sz[1], 0:sz[2]]
+    else:
+        raise ValueError('Only dimensions 1-3 are currently supported for the identity map')
+
+    # now get it into range [0,(sz-1)*spacing]^d
+    id = np.array(id.astype('float32'))
+    if dim == 1:
+        id = id.reshape(1, sz[0])  # add a dummy first index
+
+    for d in range(dim):
+        id[d] *= spacing[d]
+        id[d] -= 0.5*spacing[d]*sz[d]
+
+    # and now store it in a dim+1 array
+    if dim == 1:
+        idnp = np.zeros([1, sz[0]], dtype='float32')
+        idnp[0, :] = id[0]
+    elif dim == 2:
+        idnp = np.zeros([2, sz[0], sz[1]], dtype='float32')
+        idnp[0, :, :] = id[0]
+        idnp[1, :, :] = id[1]
+    elif dim == 3:
+        idnp = np.zeros([3, sz[0], sz[1], sz[2]], dtype='float32')
+        idnp[0, :, :, :] = id[0]
+        idnp[1, :, :, :] = id[1]
+        idnp[2, :, :, :] = id[2]
+    else:
+        raise ValueError('Only dimensions 1-3 are currently supported for the centered identity map')
+
+    return idnp
+
 
 def identity_map(sz,spacing):
     """
@@ -500,8 +572,8 @@ def space_normal(tensors, std=0.1):
             print('WARNING: What should the spacing be here? Needed for new identity map code')
             raise ValueError('Double check the spacing here before running this code')
             spacing = np.ones(dim)
-            id =identity_map(sz,spacing)
-            g = compute_normalized_gaussian(id, mus, stds)
+            centered_id = centered_identity_map(sz,spacing)
+            g = compute_normalized_gaussian(centered_id, mus, stds)
             tensors[n,c] = torch.from_numpy(g)
 
 

to_be_converted_to_tests/testGaussianFourierSmootherBackprop.py
@@ -60,8 +60,8 @@ ISource = s.smooth_scalar_field(ISource)
 
 mus = np.zeros(dim)
 stds = gaussianStd * np.ones(dim)
-id = utils.identity_map(szEx,spacing)
-g = utils.compute_normalized_gaussian(id, mus, stds)
+centered_id = utils.centered_identity_map(szEx,spacing)
+g = utils.compute_normalized_gaussian(centered_id, mus, stds)
 
 FFilter,_ = ce.create_complex_fourier_filter(g, szEx)
 fc1 = ce.FourierConvolution(FFilter)