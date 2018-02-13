67938a787f399c095eb02dc9e43ec65610ffc07f
Marc Niethammer <mn@cs.unc.edu>
2018-02-12 20:56:34 -0500

    Allows to symmetrize image registration.

--- demos/testBatchRegistration.py
+++ demos/testBatchRegistration.py
@@ -39,10 +39,19 @@ def get_image_range(im_from,im_to):
     return f
 
 # load a bunch of images as source
-I0,hdr,spacing0,_ = im_io.read_batch_to_nc_format(get_image_range(0,15))
-sz = np.array(I0.shape)
+I0_r,hdr,spacing0,_ = im_io.read_batch_to_nc_format(get_image_range(0,20))
 # and a bunch of images as target images
-I1,hdr,spacing1,_ = im_io.read_batch_to_nc_format(get_image_range(15,30))
+I1_r,hdr,spacing1,_ = im_io.read_batch_to_nc_format(get_image_range(20,40))
+
+symmetrize_images = True
+if symmetrize_images:
+    I0 = np.concatenate((I0_r,I1_r),axis=0)
+    I1 = np.concatenate((I1_r,I0_r),axis=0)
+else:
+    I0 = I0_r
+    I1 = I1_r
+
+sz = np.array(I0.shape)
 
 assert( np.all(spacing0==spacing1) )
 
@@ -53,8 +62,8 @@ reg = si.RegisterImagePair()
 if True:
     reg.register_images(I0,I1,spacing0,
                     model_name='svf_scalar_momentum_map',
-                    nr_of_iterations=50,
-                    visualize_step=5,
+                    nr_of_iterations=200,
+                    visualize_step=None,
                     map_low_res_factor=0.5,
                     rel_ftol=1e-15,
                     json_config_out_filename='testBatchNewerSmoother.json',
