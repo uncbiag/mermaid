Fixed numerical issue with weighted softmax.

pyreg/deep_smoothers.py
@@ -652,36 +652,43 @@ def weighted_softmax(input, dim=None, weights=None ):
 
     ret = torch.zeros_like(input)
 
+    # for numerical reasons we first compute the maximum inout along the dimension and then
+    # subtract if from all the exponents (this assures that we do not get exp(100) and then a NaN
+    # this is ok, because we can multiply the nominator and denominator with the same constant
+    # and by doing this shift the exponentials
+
+    max_in,_ = torch.max(input, dim=dim)
+
     if dim==0:
         norm = torch.zeros_like(input[0,...])
         for c in range(sz[0]):
-            norm += weights[c]*torch.exp(input[c,...])
+            norm += weights[c]*torch.exp(input[c,...]-max_in)
         for c in range(sz[0]):
-            ret[c,...] = weights[c]*torch.exp(input[c,...])/norm
+            ret[c,...] = weights[c]*torch.exp(input[c,...]-max_in)/norm
     elif dim==1:
         norm = torch.zeros_like(input[:,0, ...])
         for c in range(sz[1]):
-            norm += weights[c] * torch.exp(input[:,c, ...])
+            norm += weights[c] * torch.exp(input[:,c, ...]-max_in)
         for c in range(sz[1]):
-            ret[:,c, ...] = weights[c] * torch.exp(input[:,c, ...]) / norm
+            ret[:,c, ...] = weights[c] * torch.exp(input[:,c, ...]-max_in) / norm
     elif dim==2:
         norm = torch.zeros_like(input[:,:,0, ...])
         for c in range(sz[2]):
-            norm += weights[c] * torch.exp(input[:,:,c, ...])
+            norm += weights[c] * torch.exp(input[:,:,c, ...]-max_in)
         for c in range(sz[2]):
-            ret[:,:,c, ...] = weights[c] * torch.exp(input[:,:,c, ...]) / norm
+            ret[:,:,c, ...] = weights[c] * torch.exp(input[:,:,c, ...]-max_in) / norm
     elif dim==3:
         norm = torch.zeros_like(input[:,:,:,0, ...])
         for c in range(sz[3]):
-            norm += weights[c] * torch.exp(input[:,:,:,c, ...])
+            norm += weights[c] * torch.exp(input[:,:,:,c, ...]-max_in)
         for c in range(sz[3]):
-            ret[:,:,:,c, ...] = weights[c] * torch.exp(input[:,:,:,c, ...]) / norm
+            ret[:,:,:,c, ...] = weights[c] * torch.exp(input[:,:,:,c, ...]-max_in) / norm
     elif dim==4:
         norm = torch.zeros_like(input[:,:,:,:,0, ...])
         for c in range(sz[4]):
-            norm += weights[c] * torch.exp(input[:,:,:,:,c, ...])
+            norm += weights[c] * torch.exp(input[:,:,:,:,c, ...]-max_in)
         for c in range(sz[4]):
-            ret[:,:,:,:,c, ...] = weights[c] * torch.exp(input[:,:,:,:,c, ...]) / norm
+            ret[:,:,:,:,c, ...] = weights[c] * torch.exp(input[:,:,:,:,c, ...]-max_in) / norm
     else:
         raise ValueError('weighted_softmax is only supported for dimensions 0, 1, 2, 3, and 4.')
 
@@ -769,41 +776,48 @@ def weighted_sqrt_softmax(input, dim=None, weights=None ):
 
     ret = torch.zeros_like(input)
 
+    # for numerical reasons we first compute the maximum inout along the dimension and then
+    # subtract if from all the exponents (this assures that we do not get exp(100) and then a NaN
+    # this is ok, because we can multiply the nominator and denominator with the same constant
+    # and by doing this shift the exponentials
+
+    max_in, _ = torch.max(input, dim=dim)
+
     if dim==0:
         norm_sqr = torch.zeros_like(input[0,...])
         for c in range(sz[0]):
-            norm_sqr += weights[c]*(torch.exp(input[c,...]))**2
+            norm_sqr += weights[c]*(torch.exp(input[c,...]-max_in))**2
         norm = torch.sqrt(norm_sqr)
         for c in range(sz[0]):
-            ret[c,...] = torch.sqrt(weights[c])*torch.exp(input[c,...])/norm
+            ret[c,...] = torch.sqrt(weights[c])*torch.exp(input[c,...]-max_in)/norm
     elif dim==1:
         norm_sqr = torch.zeros_like(input[:,0, ...])
         for c in range(sz[1]):
-            norm_sqr += weights[c] * (torch.exp(input[:,c, ...]))**2
+            norm_sqr += weights[c] * (torch.exp(input[:,c, ...]-max_in))**2
         norm = torch.sqrt(norm_sqr)
         for c in range(sz[1]):
-            ret[:,c, ...] = torch.sqrt(weights[c]) * torch.exp(input[:,c, ...]) / norm
+            ret[:,c, ...] = torch.sqrt(weights[c]) * torch.exp(input[:,c, ...]-max_in) / norm
     elif dim==2:
         norm_sqr = torch.zeros_like(input[:,:,0, ...])
         for c in range(sz[2]):
-            norm_sqr += weights[c] * (torch.exp(input[:,:,c, ...]))**2
+            norm_sqr += weights[c] * (torch.exp(input[:,:,c, ...]-max_in))**2
         norm = torch.sqrt(norm_sqr)
         for c in range(sz[2]):
-            ret[:,:,c, ...] = torch.sqrt(weights[c]) * torch.exp(input[:,:,c, ...]) / norm
+            ret[:,:,c, ...] = torch.sqrt(weights[c]) * torch.exp(input[:,:,c, ...]-max_in) / norm
     elif dim==3:
         norm_sqr = torch.zeros_like(input[:,:,:,0, ...])
         for c in range(sz[3]):
-            norm_sqr += weights[c] * (torch.exp(input[:,:,:,c, ...]))**2
+            norm_sqr += weights[c] * (torch.exp(input[:,:,:,c, ...]-max_in))**2
         norm = torch.sqrt(norm_sqr)
         for c in range(sz[3]):
-            ret[:,:,:,c, ...] = torch.sqrt(weights[c]) * torch.exp(input[:,:,:,c, ...]) / norm
+            ret[:,:,:,c, ...] = torch.sqrt(weights[c]) * torch.exp(input[:,:,:,c, ...]-max_in) / norm
     elif dim==4:
         norm_sqr = torch.zeros_like(input[:,:,:,:,0, ...])
         for c in range(sz[4]):
-            norm_sqr += weights[c] * (torch.exp(input[:,:,:,:,c, ...]))**2
+            norm_sqr += weights[c] * (torch.exp(input[:,:,:,:,c, ...]-max_in))**2
         norm = torch.sqrt(norm_sqr)
         for c in range(sz[4]):
-            ret[:,:,:,:,c, ...] = torch.sqrt(weights[c]) * torch.exp(input[:,:,:,:,c, ...]) / norm
+            ret[:,:,:,:,c, ...] = torch.sqrt(weights[c]) * torch.exp(input[:,:,:,:,c, ...]-max_in) / norm
     else:
         raise ValueError('weighted_softmax is only supported for dimensions 0, 1, 2, 3, and 4.')
 
@@ -1200,11 +1214,12 @@ class DeepSmoothingModel(nn.Module):
         # now computed weighted TV norm channel-by-channel, square it and then take the square root (this is like in color TV)
         for g in range(self.nr_of_gaussians):
             c_local_norm_grad = _compute_local_norm_of_gradient(weights[:, g, ...], self.spacing, self.pnorm)
-            current_tv = (utils.remove_infs_from_variable(g_I * c_local_norm_grad)).sum() * self.volumeElement / batch_size
+
+            to_sum = g_I * c_local_norm_grad * self.volumeElement / batch_size
+            current_tv = (to_sum).sum()
             sum_square_of_total_variation_penalty[g] = current_tv**2
 
         total_variation_penalty = torch.norm(sum_square_of_total_variation_penalty,p=2)
-
         return total_variation_penalty
 
 class encoder_block_2d(nn.Module):