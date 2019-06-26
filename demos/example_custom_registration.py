8b01a0a723203e2d00c454fa575e4e06b4b01fe6
mn <mn@cs.unc.edu>
2019-06-25 23:16:47 -0400

    Improved example documentation.

--- demos/example_custom_registration.py
+++ demos/example_custom_registration.py
@@ -3,17 +3,41 @@
 Custom registration example
 ===========================
 
-This is a test implementation to see how we can use mermaid to implement a custom registration model and a custom
-similarity measure.
-
-Contributors:
-  Marc Niethammer: mn@cs.unc.edu
 """
 
-# Note: all images have to be in the format BxCxXxYxZ (BxCxX in 1D and BxCxXxY in 2D)
-# I.e., in 1D, 2D, 3D we are dealing with 3D, 4D, 5D tensors. B is the batchsize, and C are the channels
-# (for example to support color-images or general multi-modal registration scenarios)
+############################################################
+#
+# This tutorial whill show how to implement a custom registration model and a custom similarity measure.
+#
+# .. contents::
+#
 
+#########################
+# Introduction
+# ^^^^^^^^^^^^
+#
+# Following *pytorch* conventions all images in *mermaid* have to be in the format
+# BxCxXxYxZ (BxCxX in 1D and BxCxXxY in 2D) I.e., in 1D, 2D, 3D we are dealing with 3D, 4D, 5D tensors.
+# B is the batchsize, and C are the channels
+# (for example to support color-images or general multi-modal registration scenarios)
+#
+
+###############################3
+# Importing modules
+# ^^^^^^^^^^^^^^^^^
+#
+# We start by importing some necessary modules
+#
+# The used mermaid modules are as follows:
+#
+# - ``mermaid.example_generation`` allows to generate simply synthetic data and some real image pairs to test the registration algorithms
+# - ``mermaid.module_parameters`` allows generating mermaid parameter structures which are used to keep track of all the parameters
+# - ``mermaid.smoother_factory`` allows to generate various types of smoothers for images and vector fields
+# - ``mermaid.data_wrappter`` allows to put tensors into the right data format (for example CPU/GPU via ``AdaptVal``; makes use of the mermaid setting files for configuration)
+# - ``mermaid.multiscale_optimizer`` allows single and multi-scale optimization of all the different registration models
+# - ``mermaid.load_default_settings`` is a module from which various default settings can be loaded (without the need for providing your own json configuration file; these are written from the mermaid configuration setting files, but one can overwrite them by placing modified copies in ``.mermaid_settings``).
+
+# standard modules for str, torch, and numpy
 from __future__ import print_function
 from builtins import str
 
@@ -21,18 +45,36 @@ from builtins import str
 import torch
 import numpy as np
 
+# the mermaid modules
 import mermaid.example_generation as eg
 import mermaid.module_parameters as pars
 import mermaid.smoother_factory as SF
 from mermaid.data_wrapper import AdaptVal
 import mermaid.multiscale_optimizer as MO
-
 import mermaid.load_default_settings as ds
+
+####################################3
+# Specifying settings
+# ^^^^^^^^^^^^^^^^^^^
+#
+# Before we start we name our own new model, load some default parameters to configure the registration algorithm and settings from where to load data
+#
 
 # general parameters
 model_name = 'mySVFNet'
 
+# creating some initial parameters 
 params = pars.ParameterDict(ds.par_algconf)
+
+# these are the default parameters we loaded
+print(params)
+
+#######################
+# Creating some data
+# ^^^^^^^^^^^^^^^^^^
+#
+# We are now ready to create some data. Either synthetic or real, depending on what was specified via the configuration file.
+#
 
 if ds.load_settings_from_file:
     settingFile = 'test_custom_registration_' + model_name + '_settings.json'
@@ -61,6 +103,7 @@ print ('Spacing = ' + str( spacing ) )
 ISource = AdaptVal(torch.from_numpy( I0.copy() ))
 ITarget = AdaptVal(torch.from_numpy( I1 ))
 
+# if desired we smooth them a little bit
 if ds.smooth_images:
     # smooth both a little bit
     params['image_smoothing'] = ds.par_algconf['image_smoothing']
@@ -69,22 +112,57 @@ if ds.smooth_images:
     ISource = s.smooth(ISource)
     ITarget = s.smooth(ITarget)
 
-use_map = False # this custom registration algorithm does not use a map, so force it to False
+##############################3
+# Setting up the optimizer
+# ^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# We instantiate the multi-scale optimizer, which requires knowledge about image size and spacing,
+# as well as if a map will be used for computation (for this example we do not use one).
+#
+
+# this custom registration algorithm does not use a map, so set it to False
+use_map = False
+# If a map would be used we could compute at a lower resolution internally. 
 map_low_res_factor = None
+# Instantiate the multi-scale optimizer
 mo = MO.MultiScaleRegistrationOptimizer(sz,spacing,use_map,map_low_res_factor,params)
 
-# now customize everything
+##########################################
+# Specifiying a custom similarity measure
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# We first implement a custom similarity measure. We just pretend SSD would not already exist and reimplement it.
+#
+# Since we specify a new similarity measure we derive it from the general similiary measure class as specified in ``mermaid.similarity_measure_factory``.
+#
 
+# we name our own model ``mySSD`` and add it to the parameter object, so that the model will know which similarity measure to use
 params['registration_model']['similarity_measure']['type'] = 'mySSD'
 
 import mermaid.similarity_measure_factory as SM
 
+# this implements the similarity measure (I0Source and phi will most often not be used, but are useful in some special cases)
 class MySSD(SM.SimilarityMeasure):
     def compute_similarity(self,I0,I1, I0Source=None, phi=None):
         print('Computing my SSD')
         return ((I0 - I1) ** 2).sum() / (0.1**2) * self.volumeElement
 
-mo.add_similarity_measure('mySSD', MySSD)
+######################################
+# Specifying a registration model
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# We are now ready to specify the registration model itself. Here we implment a stationary velocity field which directly advects
+# the image (given this velocity field), i.e., no map is used.
+#
+# We make use of various mermaid modules here
+#
+# - ``mermaid.registration_networks``: This module contains all the existing registration methods as well as the base classes from which we will derive our own model.
+# - ``mermaid.utils``: This module contains various utility functions. E.g., to apply maps, deal with registration parameter dictionaries, etc.
+# - ``mermaid.image_sampling``: This module is used to resample images (for example to higher or lower resolution as needed by a multi-scale approach).
+# - ``mermaid.rungekutta_integrators``: This module contains Runge-Kutta integrators to integrate differential equations/
+# - ``mermaid.forward_models``: This module contains various forward models. For example also an advection model, which we use here.
+# - ``mermai.regularizer_factory``: This module implements various regularizers.
+#
 
 import mermaid.registration_networks as RN
 import mermaid.utils as utils
@@ -93,75 +171,166 @@ import mermaid.rungekutta_integrators as RK
 import mermaid.forward_models as FM
 import mermaid.regularizer_factory as RF
 
+# We define our own image-based SVF class
 class MySVFNet(RN.RegistrationNetTimeIntegration):
     def __init__(self,sz,spacing,params):
         super(MySVFNet, self).__init__(sz,spacing,params)
+        # we create the parameters (here simply a vector field)
         self.v = self.create_registration_parameters()
+        # and an integrator to integrate the advection equation
         self.integrator = self.create_integrator()
 
     def create_registration_parameters(self):
+        """
+        Creates the vector field over which we optimize
+        """
         return utils.create_ND_vector_field_parameter_multiN(self.sz[2::], self.nrOfImages)
 
     def get_registration_parameters(self):
+        """
+        Returns the vector field over which we optimize
+        """
         return self.v
 
     def set_registration_parameters(self, p, sz, spacing):
+        """
+        Allows setting the registration parameters. This is neede for the multi-scale solver to go between the scales.
+
+        :param p: Registration parameter (here the vector field)
+        :param sz: size at current scale
+        :param spacing: spacing at current scale
+        :return: n/a
+        """
         self.v.data = p.data
         self.sz = sz
         self.spacing = spacing
 
     def create_integrator(self):
+        """
+        Creates an instance of the integrator (here RK4)
+        """
+        # here is where parameters for the forward model are always stored
         cparams = self.params[('forward_model',{},'settings for the forward model')]
+        # we create an advection forward model
         advection = FM.AdvectImage(self.sz, self.spacing)
+        # create the parameters to be passed to the integrator as a dictionary (including default parameters that can be passed)
         pars_to_pass = utils.combine_dict({'v': self.v}, self._get_default_dictionary_to_pass_to_integrator())
+        # now we create the integrator and return it
         return RK.RK4(advection.f, advection.u, pars_to_pass, cparams)
 
     def forward(self, I, variables_from_optimizer=None):
+        """
+        The forward method simply applies the current registration transform (here integrates the advection equation)
+
+        :param I: input image
+        :param variable_from_optimizer: additional parameters that can be passed from the optimizer
+        :return: returns the warped image I
+        """
+
+        # as we derived our class from RegistrationNetTimeIntegration we have access to member variables tFrom and tTo
+        # specifying the desired integration time interval
         I1 = self.integrator.solve([I], self.tFrom, self.tTo)
         return I1[0]
 
     def upsample_registration_parameters(self, desiredSz):
+        """
+        Upsamples the registration parameters (needed for multi-scale solver).
+
+        :param desiredSz: desired size to which we want to upsample.
+        :return: returns the upsampled parameter (the upsampled velocity field) and the corresponding spacing as a tuple
+        """
         sampler = IS.ResampleImage()
         vUpsampled,upsampled_spacing=sampler.upsample_image_to_size(self.v,self.spacing,desiredSz,spline_order=1)
         return vUpsampled,upsampled_spacing
 
     def downsample_registration_parameters(self, desiredSz):
+        """
+        Downsamples the registration parameters (needed for multi-scale solver).
+
+        :param desiredSz: desired size to which we want to downsample.
+        :return: returns the downsampled parameter (the upsampled velocity field) and the corresponding spacing as a tuple
+        """
         sampler = IS.ResampleImage()
         vDownsampled,downsampled_spacing=sampler.downsample_image_to_size(self.v,self.spacing,desiredSz,spline_order=1)
         return vDownsampled,downsampled_spacing
 
+###################################
+# Specifying the loss function
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Lastly, we write out custom loss function to penalize deformations that are not smooth and image mismatch.
+# As we already defined the similarity measure (which will get called behind the scenes) we only need to deal
+# with implementing the regularization energy here.
+#
+# We derive this class from ``RegistrationImageLoss``.
+#
+    
 class MySVFImageLoss(RN.RegistrationImageLoss):
     def __init__(self,v,sz_sim,spacing_sim,sz_model,spacing_model,params):
         super(MySVFImageLoss, self).__init__(sz_sim,spacing_sim,sz_model,spacing_model,params)
+        # the registration parameters
         self.v = v
+        # create a new parameter category
         cparams = params[('loss',{},'settings for the loss function')]
+        # under this category the regularizer settings will be saved
         self.regularizer = (RF.RegularizerFactory(self.spacing_sim).
                             create_regularizer(cparams))
 
     def compute_regularization_energy(self, I0_source, variables_from_forward_model=None, variables_from_optimizer=False):
+        # here we compute the regularization energy, by simply evaluating the regularizer
         return self.regularizer.compute_regularizer_multiN(self.v)
 
+#############################################
+# Multi-scale optimizing our own custom model
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# We are now ready to optimize over our model. The only thing left is to associate the similarity measure, the registration model, and the loss
+# function with the multi-scale optimizer. Note that we only had to implement upsampling and downsampling methods for the registration parameters.
+# THe entire multi-scale framework now comes for free
+#
+    
+# We have now created our own similiary measure (``MySSD``). We know register it with the multi-scale optimizer, so it knows it exists.
+mo.add_similarity_measure('mySSD', MySSD)
+
+# We do the same with the registration model and its loss
 mo.add_model(model_name,MySVFNet,MySVFImageLoss,use_map=False)
+# and set the model name, so we can refer to it
 mo.set_model(model_name)
 
+# here we set some visualization options (if to visualize and how frequently)
 mo.set_visualization( ds.visualize )
 mo.set_visualize_step( ds.visualize_step )
 
+# we let the optimizer know what is the source and what is the target image
 mo.set_source_image(ISource)
 mo.set_target_image(ITarget)
 
+# we set the scale factors and the number of iterations
 mo.set_scale_factors( ds.multi_scale_scale_factors )
 mo.set_number_of_iterations_per_scale( ds.multi_scale_iterations_per_scale )
 
-# now we also pick a custom optimizer
+# and while we are at it we also pick a custom optimizer
 mo.set_optimizer(torch.optim.Adam)
 mo.set_optimizer_params(dict(lr=0.01))
 
-# and now do the optimization
+# and now we do the optimization
 mo.set_light_analysis_on(True)
 
 mo.optimize()
 
+# If desired we can write out the registration parameters. Which keeps track of all parameters that were used.
+# These parameters are as follows (and we can see that out own model was used).
+#
+
+print(params)
+
 if ds.save_settings_to_file:
     params.write_JSON( 'test_custom_registration_' + model_name + '_settings_clean.json')
     params.write_JSON_comments( 'test_custom_registration_' + model_name + '_settings_comments.json')
+
+###########################################3
+# Conclusion
+# ^^^^^^^^^^
+#
+# Given this example it is possible to set up a custom registration model without too much overhead and without being exposed to the complex *mermaid* internals.
+#
--- demos/example_simple_interface.py
+++ demos/example_simple_interface.py
@@ -1,10 +1,42 @@
+# -*- coding: utf-8 -*-
 """
 
 Simple registration interface example
 =====================================
 
-This script shows how to use mermaid's simple registration interface.
 """
+
+
+######################################################################
+# 
+# This tutorial will show how to use the simple registration interface of *mermaid*.
+#
+# .. contents::
+#
+
+
+##############################
+# Introduction
+# ^^^^^^^^^^^^
+#
+# Mermaid can be a rather complex registration package.
+# Hence it provides various convenience functions to make registration reasonably easy.
+# One of them is a simple registration interface that allows setting the most popular parameters.
+# But it also allows passing through all the advanced registration paramers as desired.
+#
+
+##############################
+# Importing modules
+# ^^^^^^^^^^^^^^^^^
+#
+# We start by importing some popular modules
+#
+# The used mermaid modules are as follows:
+#
+# - ``mermaid.simple_interface`` provides a very high level interface to the image registration algorithms
+# - ``mermaid.example_generation`` allows to generate simply synthetic data and some real image pairs to test the registration algorithms
+# - ``mermaid.module_parameters`` allows generating mermaid parameter structures which are used to keep track of all the parameters
+#
 
 from __future__ import print_function
 
@@ -15,110 +47,91 @@ import mermaid.simple_interface as SI
 import mermaid.example_generation as EG
 import mermaid.module_parameters as pars
 
-# keep track of general parameters
+################################
+# Creating some example data
+#
+# We can use ``mermaid.example_generation`` to create some test data
+#
+
+# We first generate a parameter object to hold settings
 params = pars.ParameterDict()
 
+# We select if we want to create synhetic image pairs or would prefer a real image pair
 use_synthetic_test_case = True
+
+# Desired dimension (mermaid supports 1D, 2D, and 3D registration)
 dim = 2
-try_all_models = False
+
+# If we want to add some noise to the background (for synthetic examples)
 add_noise_to_bg = True
+
+# and now create it
 if use_synthetic_test_case:
     len = 64
-    szEx = np.tile( len, dim )         # size of the desired images: (sz)^dim
-    I0,I1,spacing= EG.CreateSquares(dim,add_noise_to_bg).create_image_pair(szEx,params) # create a default image size with two sample squares
+    # size of the desired images: (sz)^dim
+    szEx = np.tile( len, dim )         
+    # create a default image size with two sample squares
+    I0,I1,spacing= EG.CreateSquares(dim,add_noise_to_bg).create_image_pair(szEx,params) 
 else:
+    # return a real image example
     I0,I1,spacing = EG.CreateRealExampleImages(dim).create_image_pair() # create a default image size with two sample squares
 
-"""
-Known registration models are:
-------------------------------
-                     total_variation_map: displacement-based total variation registration
-                 svf_vector_momentum_map: map-based stationary velocity field using the vector momentum
-               svf_vector_momentum_image: image-based stationary velocity field using the vector momentum
-                              affine_map: map-based affine registration
-                 svf_scalar_momentum_map: map-based stationary velocity field using the scalar momentum
-                           curvature_map: displacement-based curvature registration
-                    lddmm_shooting_image: image-based shooting-based LDDMM using the vector momentum
-                           diffusion_map: displacement-based diffusion registration
-                               svf_image: image-based stationary velocity field
-                                 svf_map: map-based stationary velocity field
-               svf_scalar_momentum_image: image-based stationary velocity field using the scalar momentum
-                svf_quasi_momentum_image: EXPERIMENTAL: Image-based SVF version which estimates velcocity based on a smoothed vetor field
-    lddmm_shooting_scalar_momentum_image: image-based shooting-based LDDMM using the scalar momentum
-      lddmm_shooting_scalar_momentum_map: map-based shooting-based LDDMM using the scalar momentum
-                      lddmm_shooting_map: map-based shooting-based LDDMM using the vector momentum
-"""
+##################################
+# Creating the registration algorithm
+#
+# We simply instantiate a simple interface object for the registration of image pairs.
+# We can then query it as to what models registration models are currently supported.
+#
 
-# print possible model names
+# create a simple interface object for pair-wise image registration
 si = SI.RegisterImagePair()
 
+# print possible model names
 si.print_available_models()
-all_models = si.get_available_models()
 
-if try_all_models:
-    # try all the models
-    for model_name in all_models:
-        print('Registering with model: ' + model_name )
-        si.register_images(I0, I1, spacing, model_name=model_name)
-else:
-    # just try one of them, explicitly specified
-    #si.register_images(I0, I1, spacing, model_name='total_variation_map')
-    #si.register_images(I0, I1, spacing, model_name='svf_vector_momentum_map',nr_of_iterations=30,optimizer_name='sgd',compute_inverse_map=True)
+################################
+# Doing the registration
+# ^^^^^^^^^^^^^^^^^^^^^^
+#
+# We are now ready to perform the registration (picking one of the registration model options printed above).
+#
+# Here, we use a shooting-based LDDMM algorithm which works directly with transformation maps.
+# The simple interface allows setting various registration settings.
+#
+# Of note, we read in a parameter file (``test2d_tst.json``) to parameterize the registration algorithm and
+# write out the used parameters (after the run) into the same file as well as into a file with comments explaining
+# all the parameter settings. If ``params`` is not specified default settings will be used. By running for one
+# iteration for example, this allows writing out a fresh settings template which can then be edited.
+#
 
-    # si.register_images(I0, I1, spacing, model_name='svf_vector_momentum_map',
-    #                    nr_of_iterations=50,
-    #                    visualize_step=10,
-    #                    optimizer_name='sgd',
-    #                    rel_ftol=1e-7,
-    #
-    #                    json_config_out_filename = 'test2d_tst.json',
-    #                    params = 'test2d_tst.json')
 
-    si.register_images(I0, I1, spacing, model_name='lddmm_shooting_map',
+si.register_images(I0, I1, spacing, model_name='lddmm_shooting_map',
                        nr_of_iterations=50,
                        use_multi_scale=False,
                        visualize_step=10,
                        optimizer_name='sgd',
-                       learning_rate=0.02,
+                       learning_rate=0.1,
                        rel_ftol=1e-7,
                        json_config_out_filename=('test2d_tst.json','test2d_tst_with_comments.json'),
                        params='test2d_tst.json')
 
-    #si.register_images(I0, I1, spacing, model_name='svf_vector_momentum_image')
-    #si.register_images(I0, I1, spacing, model_name='affine_map')
-    #si.register_images(I0, I1, spacing, model_name='svf_scalar_momentum_map',
-    #                                       smoother_type='adaptive_multiGaussian',
-    #                                       optimize_over_smoother_parameters=True,
-    #                                       map_low_res_factor=1.0,
-    #                                       visualize_step=10,
-    #                                       nr_of_iterations=10,
-    #                                       rel_ftol=1e-8,
-    #                                       similarity_measure_sigma=0.01)
-    #si.register_images(I0, I1, spacing, model_name='curvature_map',rel_ftol=1e-12, similarity_measure_sigma=0.005,nr_of_iterations=100)
-    #si.register_images(I0, I1, spacing, model_name='lddmm_shooting_image',nr_of_iterations=100)
-    #si.register_images(I0, I1, spacing, model_name='diffusion_map')
-    #si.register_images(I0, I1, spacing, model_name='svf_image')
-    #si.register_images(I0, I1, spacing, model_name='svf_map')
-    #si.register_images(I0, I1, spacing, model_name='svf_scalar_momentum_image')
-    #si.register_images(I0, I1, spacing, model_name='svf_quasi_momentum_image',nr_of_iterations=100)
-    #si.register_images(I0, I1, spacing, model_name='lddmm_shooting_scalar_momentum_image',nr_of_iterations=10,use_multi_scale=False)
-    #si.register_images(I0, I1, spacing, model_name='lddmm_shooting_scalar_momentum_map')
-    #si.register_images(I0, I1, spacing, model_name='lddmm_shooting_map')
+############################
+# Plotting some results
+# ^^^^^^^^^^^^^^^^^^^^^
+#
+# We can query the energies over the iterations. Note that this code need to be modified for a multi-scale solution as
+# energies will be returned at each scale
+#
 
-    #si.register_images(I0, I1, spacing, model_name='svf_scalar_momentum_map',
-    #                    visualize_step=5,
-    #                    nr_of_iterations=20,
-    #                    similarity_measure_type='omt')
+h = si.get_history()
 
-    h = si.get_history()
+plt.clf()
 
-    plt.clf()
-
-    e_p, = plt.plot(h['energy'], label='energy')
-    s_p, = plt.plot(h['similarity_energy'], label='similarity_energy')
-    r_p, = plt.plot(h['regularization_energy'], label='regularization_energy')
-    plt.legend(handles=[e_p,s_p,r_p])
-    plt.show()
+e_p, = plt.plot(h['energy'], label='energy')
+s_p, = plt.plot(h['similarity_energy'], label='similarity_energy')
+r_p, = plt.plot(h['regularization_energy'], label='regularization_energy')
+plt.legend(handles=[e_p,s_p,r_p])
+plt.show()
 
 
 
