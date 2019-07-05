d182efb64195524c04f5203d72336e7da8d090bb
mn <mn@cs.unc.edu>
2019-07-05 11:32:25 +0200

    Cleaned up documentation for example codes.

--- demos/example_minimal_registration_without_simple_interface.py
+++ demos/example_minimal_registration_without_simple_interface.py
@@ -3,10 +3,38 @@
 Minimal registration example
 ============================
 
-This example script is a very minimialistic example for using mermaid.
 """
 
-# first do the torch imports
+######################################################
+#
+# This tutorial will walk you through a very minimalistic example for image registration via mermaid.
+#
+# .. contents::
+#
+
+##########################
+# Introduction
+# ^^^^^^^^^^^^
+#
+# There are essentially three different ways of how to do registration with pytorch.
+# Below are these options in increasing level of difficulty, but in order of increasing flexibility.
+#
+# 1. Use the simple registration interface (see ``example_simple_interface.py`` in the demos directory.
+# 2. Manually instantiating the mermaid optimizer and setting up the registration parameters (which we are going to do here).
+# 3. Instantiating the mermaid registration models and writing your own optimizer for them.
+#
+
+##########################
+# Importing modules
+# ^^^^^^^^^^^^^^^^^
+#
+# We start by importing some popular modules. The used mermaid modules are:
+#
+# - ``mermaid.multiscale_optimizer`` provides single scale and multi-scale optimizing functionality for all the registration models.
+# - ``mermaid.example_generation`` allows to generate simply synthetic data and some real image pairs to test the registration algorithms
+# - ``mermaid.module_parameters`` allows generating mermaid parameter structures which are used to keep track of all the parameters
+#
+
 import torch
 from mermaid.data_wrapper import AdaptVal
 import numpy as np
@@ -15,31 +43,80 @@ import mermaid.example_generation as eg
 import mermaid.module_parameters as pars
 import mermaid.multiscale_optimizer as MO
 
+############################
+# Specifying registration model properties
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Next we specify some settings which will need to be specified when setting up the registration model.
+# These are the meanings of the following variables:
+#
+# - ``use_map``: Most image registration models in mermaid (and all the ones that are based on integration through velocity fields) support solutions by directly solving the associated equations on the images or alternatively via evolution of the transformation map (if ``use_map=True``). In general, a map-based solution is preferred as it avoids numerical discretization issues when, for example, directly advecting an image and because it directly results in the desired transformation map. Note that within mermaid a map phi denotes a map in target space (i.e., to map from source to target space) and its inverse map is a map defined in the coordinates of the source image (to map from the target space to the source space).
+# - ``model_name``: Simply the name of the desired registration model. For example, ``lddmm_shooting`` is an LDDMM implementation which optimizes over the initial vector momentum and operates either on maps or directly on the image (depening on ``use_map``).
+# - ``map_low_res_factor``: Especially when computing in 3D memory sometimes becomes an issue. Hence, for map-based solutions mermaid supports computing solutions of the evolution equations at a lower resolution. ``map_low_res_factor`` specifies how low this resoultion should be. ``None`` or ``1.0`` is the original resolution. ``0.5``, for example, uses half the resolution in each direction. In any case, the similarity measure is always evaluated at the original resolution via upsampling of the map.
+
 use_map = True
 model_name = 'lddmm_shooting'
-dim = 2
-example_img_len = 64
 map_low_res_factor = 0.5
-optimizer_name = 'sgd'
-visualize = True
-visualize_step = 10
-nr_of_iterations = 50
 
 if use_map:
     model_name = model_name + '_map'
 else:
     model_name = model_name + '_image'
 
+
+############################
+# Specifying the optimizer
+# ^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# *mermaid* support mostly ``sgd`` or ``lbfgs_ls`` (an LBFGS method with linesearch). Other optimizers can be used, but this is not well supported at the moment. As each of these optimizers iterates the number of desired iterations needs to be specified. The optimizer also suppport visualizing intermediate output at given iteration intervals.
+#
+
+optimizer_name = 'sgd'
+nr_of_iterations = 50
+visualize = True
+visualize_step = 10
+
+#########################
+# Creating the parameter structure
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# AS typical in mermaid, we create a parameters structure (``params``) that will keep track of all the parameters used during a run.
+#
+
 # keep track of general parameters
 params = pars.ParameterDict()
+
+#############################
+# Creating an example
+# ^^^^^^^^^^^^^^^^^^^
+#
+# Now we are ready to create some example data. We create squares for source and target images in two dimensions.
+
+dim = 2
+example_img_len = 64
 
 szEx = np.tile( example_img_len, dim )         # size of the desired images: (sz)^dim
 I0,I1,spacing= eg.CreateSquares(dim).create_image_pair(szEx,params) # create a default image size with two sample squares
 sz = np.array(I0.shape)
 
+##############################
+# Moving from numpy to pytorch
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# The example generation produces numpy arrays. As *mermaid* uses pytorch these need to be converted to pytorch arrays. Also, we support running *mermaid* on the CPU and the GPU. The convenience function ``AdaptVal`` takes care of this by moving an array either to the GPU or leaving it on the CPU.
+
 # create the source and target image as pyTorch variables
 ISource = AdaptVal(torch.from_numpy(I0.copy()))
 ITarget = AdaptVal(torch.from_numpy(I1))
+
+#############################
+# Instantiating the optimizer
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Now we are ready to instantiate the optimizer with the parameters defined above.
+# We instantiate a single-scale optimizer here, but instantiating a multi-scale optimizer
+# proceeds similarly. We then start the optimization (via ``so.optimizer()``).
+#
 
 so = MO.SingleScaleRegistrationOptimizer(sz,spacing,use_map,map_low_res_factor,params)
 so.set_model(model_name)
@@ -55,6 +132,15 @@ so.set_light_analysis_on(True)
 
 # and now do the optimization
 so.optimize()
+
+##############################
+# Writing out the parameter configuration file
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# To inspect what settings were in fact used during the run we can write them out using json.
+# We can write out both the actual settings as well as a json file describing what the individual
+# settings mean. If desired one can then modify these settings and load them back in as the
+# new desired setting for a new optimization run.
 
 params.write_JSON( 'test_minimal_registration_' + model_name + '_settings_clean.json')
 params.write_JSON_comments( 'test_minimal_registration_' + model_name + '_settings_comments.json')
--- demos/example_step_by_step_registration.py
+++ demos/example_step_by_step_registration.py
@@ -3,11 +3,39 @@
 Step-by-step registration example
 =================================
 
-This example script walks through a mermaid registration example step by step.
 """
 
+#####################################
+#
+# This tutorial will take you step-by-step through a very simple registration example.
+#
+# .. contents::
+#
 
-# before we start let's import some mermaid libraries
+
+######################################
+# Introduction
+# ^^^^^^^^^^^^
+#
+# Starting with *mermaid* can initially be a little overwhelming. We therefore walk through a simple step-by-step registration example here. This example should teach you:
+#
+# - how to import the most essential *mermaid* packages
+# - how to generate some example data
+# - how to run instantiate and run a simple registration
+# - how to work with mermaid parameter structures (these are important as they keep track of all the mermaid settings and can also be edited)
+#
+
+##################################
+# Importing modules
+# ^^^^^^^^^^^^^^^^^
+#
+# First we import some important mermaid modules. The used mermaid modules are:
+#
+# - ``mermaid.simple_interface`` provides a very high level interface to the image registration algorithms
+# - ``mermaid.example_generation`` allows to generate simply synthetic data and some real image pairs to test the registration algorithms
+# - ``mermaid.module_parameters`` allows generating mermaid parameter structures which are used to keep track of all the parameters
+#
+# Some of the high-level mermaid code and also the plotting depends on numpy (we will phase much of this out in the future). Hence we also ``import numpy``.
 
 # first the simple registration interface (which provides basic, easy to use registration functionality)
 import mermaid.simple_interface as SI
@@ -19,20 +47,60 @@ import mermaid.example_generation as EG
 # also import numpy
 import numpy as np
 
+##########################
+# mermaid parameters
+# ^^^^^^^^^^^^^^^^^^
+#
+# Registration algorithms tend to have a lot of settings. Starting from the registration model, over
+# the selection and settings for the optimizer, to general compute settings (for example, if mermaid
+# should be run on the GPU or CPU). All the non-compute settings that affect registration results are
+# automatically kept track inside a parameters structure.
+#
+# So let's first create an empty *mermaid* parameter object
+
 # first we create a parameter structure to keep track of all registration settings
 params = pars.ParameterDict()
 
+#############################
+# Generating registration example data
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Now we create some example data (source and target image examples for a two-dimensional square,
+# of size 64x64) and keep track of the generated settings via this parameter object.
+
 # and let's create two-dimensional squares
 I0,I1,spacing = EG.CreateSquares(dim=2,add_noise_to_bg=True).create_image_pair(np.array([64,64]),params=params)
+
+###############################
+# Writing out parameters
+# ^^^^^^^^^^^^^^^^^^^^^^
+#
+# Parameters can easily be written to a file (or displayed via print). We can write it out including comments for what these settings are as follows. The first command writes out the actual json configuration, the second one comments that explain what the settings are (as json does not allow commented files by default).
+
 params.write_JSON('step_by_step_example_data.json')
 params.write_JSON_comments('step_by_step_example_data_with_comments.json')
+
+###############################
+# Resulting parameters after the example generation
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# The resulting output in this case looks as follows. This is contained in the files, but we simply print the
+# parameters here.
+
+print(params)
+
+#################################
+# Performing the registration
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Now we are ready to instantiate the registration object from mermaid. As we are not sure what settings to use, let alone know what settings exist, we simply run it first for one step and ask for the json configuration to be written out.
 
 # now we instantiate the registration class
 si = SI.RegisterImagePair()
 
 # and use it for registration
 # as registration algorithms can have many settings we simply run it first for one iteration and ask it to
-# write out a configuration file, including comments to explain the settings
+# write out a configuration file, including comments to explain the settings.
 si.register_images(I0, I1, spacing,
                    model_name='lddmm_shooting_map',
                    nr_of_iterations=1,
