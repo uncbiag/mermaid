17d2015666cd228df25269778ce227bf9837150a
HastingsGreer <hastings.greer@gmail.com>
2021-11-14 10:51:12 -0500

    Config files into wheel (#146)
    *get wheel to include config files

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    package_data={'mermaid_apps': ['*.sh']},
+    package_data={'mermaid_apps': ['*.sh'], "mermaid_settings": ['*.json']},
                   
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
